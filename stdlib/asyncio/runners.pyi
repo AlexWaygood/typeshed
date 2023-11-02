d75f0bf73acbab74ceb80a59150b04da6aa336e2
AlexWaygood <alex.waygood@gmail.com>
2023-11-02 17:35:19 +0000

    Run stubdefaulter

--- stdlib/asyncio/runners.pyi
+++ stdlib/asyncio/runners.pyi
@@ -25,7 +25,7 @@ if sys.version_info >= (3, 11):
 
 if sys.version_info >= (3, 12):
     def run(
-        main: Coroutine[Any, Any, _T], *, debug: bool | None = ..., loop_factory: Callable[[], AbstractEventLoop] | None = ...
+        main: Coroutine[Any, Any, _T], *, debug: bool | None = None, loop_factory: Callable[[], AbstractEventLoop] | None = None
     ) -> _T: ...
 
 elif sys.version_info >= (3, 8):
