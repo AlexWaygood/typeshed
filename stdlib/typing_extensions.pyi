e11467613e66ee044e1c04ea4b6e855a50f72ada
AlexWaygood <alex.waygood@gmail.com>
2023-11-28 15:40:32 +0000

    repros

--- stdlib/typing_extensions.pyi
+++ stdlib/typing_extensions.pyi
@@ -2,67 +2,29 @@ import abc
 import typing
 from collections.abc import Buffer as Buffer
 from types import GenericAlias, UnionType, get_original_bases as get_original_bases
-from typing import (  # noqa: Y022,Y037,Y038
+from typing import (  # noqa: Y022,Y038
     IO as IO,
     TYPE_CHECKING as TYPE_CHECKING,
     AbstractSet as AbstractSet,
     Any as Any,
     AnyStr as AnyStr,
-    AsyncGenerator as AsyncGenerator,
-    AsyncIterable as AsyncIterable,
-    AsyncIterator as AsyncIterator,
-    Awaitable as Awaitable,
-    BinaryIO as BinaryIO,
-    Callable as Callable,
-    ClassVar as ClassVar,
-    Collection as Collection,
-    Concatenate as Concatenate,
-    Container as Container,
-    Coroutine as Coroutine,
-    Dict as Dict,
-    ForwardRef as ForwardRef,
-    Generator as Generator,
-    Generic as Generic,
-    Hashable as Hashable,
-    ItemsView as ItemsView,
-    Iterable as Iterable,
-    Iterator as Iterator,
-    KeysView as KeysView,
-    List as List,
+    Callable,
+    ClassVar,
+    ItemsView,
+    KeysView,
     LiteralString as LiteralString,
-    Mapping as Mapping,
-    MappingView as MappingView,
-    MutableMapping as MutableMapping,
-    MutableSequence as MutableSequence,
-    MutableSet as MutableSet,
-    NamedTuple as NamedTuple,
+    Mapping,
     Never as Never,
     NewType as NewType,
     NoReturn as NoReturn,
     NotRequired as NotRequired,
-    Optional as Optional,
     Required as Required,
-    Reversible as Reversible,
     Self as Self,
-    Sequence as Sequence,
-    Set as Set,
-    Sized as Sized,
-    SupportsAbs as SupportsAbs,
-    SupportsBytes as SupportsBytes,
-    SupportsFloat as SupportsFloat,
-    SupportsInt as SupportsInt,
-    SupportsRound as SupportsRound,
-    TextIO as TextIO,
-    Tuple as Tuple,
-    Type as Type,
     TypeAlias as TypeAlias,
     TypeGuard as TypeGuard,
-    Union as Union,
     Unpack as Unpack,
     ValuesView as ValuesView,
-    _Alias,
-    cast as cast,
-    overload as overload,
+    overload,
     type_check_only,
 )
 
@@ -114,8 +76,6 @@ class _TypedDict(Mapping[str, object], metaclass=abc.ABCMeta):
 # TypedDict is a (non-subscriptable) special form.
 TypedDict: object
 
-OrderedDict = _Alias()
-
 def get_type_hints(
     obj: Callable[..., Any],
     globalns: dict[str, Any] | None = None,
@@ -131,38 +91,3 @@ def get_origin(tp: GenericAlias) -> type: ...
 def get_origin(tp: Any) -> Any | None: ...
 
 Annotated: _SpecialForm
-_AnnotatedAlias: Any  # undocumented
-
-@runtime_checkable
-class SupportsIndex(Protocol, metaclass=abc.ABCMeta):
-    @abc.abstractmethod
-    def __index__(self) -> int: ...
-
-class TypeVar:
-    @property
-    def __name__(self) -> str: ...
-    @property
-    def __bound__(self) -> Any | None: ...
-    @property
-    def __constraints__(self) -> tuple[Any, ...]: ...
-    @property
-    def __covariant__(self) -> bool: ...
-    @property
-    def __contravariant__(self) -> bool: ...
-    @property
-    def __infer_variance__(self) -> bool: ...
-    @property
-    def __default__(self) -> Any | None: ...
-    def __init__(
-        self,
-        name: str,
-        *constraints: Any,
-        bound: Any | None = None,
-        covariant: bool = False,
-        contravariant: bool = False,
-        default: Any | None = None,
-        infer_variance: bool = False,
-    ) -> None: ...
-
-class ParamSpec: ...
-class TypeVarTuple: ...
