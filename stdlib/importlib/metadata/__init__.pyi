2f341ac80c6b731085ac9cda07e5d1b901822158
AlexWaygood <alex.waygood@gmail.com>
2023-11-28 14:00:45 +0000

    repros

--- stdlib/importlib/metadata/__init__.pyi
+++ stdlib/importlib/metadata/__init__.pyi
@@ -3,87 +3,9 @@ import sys
 from _typeshed import StrPath
 from collections.abc import Iterable
 from importlib.abc import MetaPathFinder
-from typing import Any, ClassVar, NamedTuple, overload
-from typing_extensions import Self
+from typing import Any, overload
 
-class _EntryPointBase(NamedTuple):
-    name: str
-    value: str
-    group: str
-
-class EntryPoint(_EntryPointBase):
-    if sys.version_info >= (3, 11):
-        def __init__(self, name: str, value: str, group: str) -> None: ...
-
-    def load(self) -> Any: ...  # Callable[[], Any] or an importable module
-    @property
-    def extras(self) -> list[str]: ...
-    if sys.version_info >= (3, 9):
-        @property
-        def module(self) -> str: ...
-        @property
-        def attr(self) -> str: ...
-    if sys.version_info >= (3, 10):
-        dist: ClassVar[Distribution | None]
-        def matches(
-            self,
-            *,
-            name: str = ...,
-            value: str = ...,
-            group: str = ...,
-            module: str = ...,
-            attr: str = ...,
-            extras: list[str] = ...,
-        ) -> bool: ...  # undocumented
-
-    def __hash__(self) -> int: ...
-    def __eq__(self, other: object) -> bool: ...
-
-if sys.version_info >= (3, 10):
-    class EntryPoints(list[EntryPoint]):  # use as list is deprecated since 3.10
-        # int argument is deprecated since 3.10
-        def __getitem__(self, name: int | str) -> EntryPoint: ...  # type: ignore[override]
-        def select(
-            self,
-            *,
-            name: str = ...,
-            value: str = ...,
-            group: str = ...,
-            module: str = ...,
-            attr: str = ...,
-            extras: list[str] = ...,
-        ) -> EntryPoints: ...
-        @property
-        def names(self) -> set[str]: ...
-        @property
-        def groups(self) -> set[str]: ...
-
-if sys.version_info >= (3, 10) and sys.version_info < (3, 12):
-    class SelectableGroups(dict[str, EntryPoints]):  # use as dict is deprecated since 3.10
-        @classmethod
-        def load(cls, eps: Iterable[EntryPoint]) -> Self: ...
-        @property
-        def groups(self) -> set[str]: ...
-        @property
-        def names(self) -> set[str]: ...
-        @overload
-        def select(self) -> Self: ...  # type: ignore[misc]
-        @overload
-        def select(
-            self,
-            *,
-            name: str = ...,
-            value: str = ...,
-            group: str = ...,
-            module: str = ...,
-            attr: str = ...,
-            extras: list[str] = ...,
-        ) -> EntryPoints: ...
-
-class FileHash:
-    mode: str
-    value: str
-    def __init__(self, spec: str) -> None: ...
+class EntryPoints(list[Any]): ...
 
 class Distribution:
     @abc.abstractmethod
@@ -103,16 +25,11 @@ class Distribution:
     @property
     def metadata(self) -> Any: ...
     @property
-    def entry_points(self) -> list[EntryPoint]: ...
-    @property
     def version(self) -> str: ...
     @property
     def files(self) -> None: ...
     @property
     def requires(self) -> list[str] | None: ...
-    if sys.version_info >= (3, 10):
-        @property
-        def name(self) -> str: ...
 
 class DistributionFinder(MetaPathFinder):
     class Context:
