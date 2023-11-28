19b6d06c84748eeb9a872426b0c388c0c70af75b
AlexWaygood <alex.waygood@gmail.com>
2023-11-28 13:41:04 +0000

    repros

--- stdlib/_typeshed.pyi
+++ stdlib/_typeshed.pyi
@@ -56,43 +56,6 @@ StrPath: TypeAlias = str  # stable
 BytesPath: TypeAlias = bytes  # stable
 StrOrBytesPath: TypeAlias = str | bytes  # stable
 
-OpenTextModeUpdating: TypeAlias = Literal[
-    "r+",
-    "+r",
-    "rt+",
-    "r+t",
-    "+rt",
-    "tr+",
-    "t+r",
-    "+tr",
-    "w+",
-    "+w",
-    "wt+",
-    "w+t",
-    "+wt",
-    "tw+",
-    "t+w",
-    "+tw",
-    "a+",
-    "+a",
-    "at+",
-    "a+t",
-    "+at",
-    "ta+",
-    "t+a",
-    "+ta",
-    "x+",
-    "+x",
-    "xt+",
-    "x+t",
-    "+xt",
-    "tx+",
-    "t+x",
-    "+tx",
-]
-OpenTextModeWriting: TypeAlias = Literal["w", "wt", "tw", "a", "at", "ta", "x", "xt", "tx"]
-OpenTextModeReading: TypeAlias = Literal["r", "rt", "tr", "U", "rU", "Ur", "rtU", "rUt", "Urt", "trU", "tUr", "Utr"]
-OpenTextMode: TypeAlias = OpenTextModeUpdating | OpenTextModeWriting | OpenTextModeReading
 OpenBinaryModeUpdating: TypeAlias = Literal[
     "rb+",
     "r+b",
