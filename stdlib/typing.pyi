4652884e7302f05da4b22a859665de6a5d1a66d3
AlexWaygood <alex.waygood@gmail.com>
2023-11-28 18:15:40 +0000

    repros

--- stdlib/typing.pyi
+++ stdlib/typing.pyi
@@ -1,12 +1,9 @@
 import abc  # noqa: F401
 import collections  # noqa: F401
 
-Any = object()
-
 class TypeVar: ...
 class _SpecialForm: ...
 
 Generic: _SpecialForm
 Callable: _SpecialForm
-Literal: _SpecialForm
 TypeAlias: _SpecialForm
