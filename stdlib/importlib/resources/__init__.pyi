6122815bacf25003bc8ef02d2667cf6618693694
AlexWaygood <alex.waygood@gmail.com>
2023-11-28 13:56:13 +0000

    repros

--- stdlib/importlib/resources/__init__.pyi
+++ stdlib/importlib/resources/__init__.pyi
@@ -1,11 +1,7 @@
-import sys
-from collections.abc import Iterator
+from importlib.abc import Traversable
 from types import ModuleType
 from typing import BinaryIO, TextIO
 from typing_extensions import TypeAlias
-
-if sys.version_info >= (3, 9):
-    from importlib.abc import Traversable
 
 Package: TypeAlias = str | ModuleType
 Resource: TypeAlias = str
@@ -14,14 +10,6 @@ def open_binary(package: Package, resource: Resource) -> BinaryIO: ...
 def open_text(package: Package, resource: Resource, encoding: str = "utf-8", errors: str = "strict") -> TextIO: ...
 def read_binary(package: Package, resource: Resource) -> bytes: ...
 def read_text(package: Package, resource: Resource, encoding: str = "utf-8", errors: str = "strict") -> str: ...
-def is_resource(package: Package, name: str) -> bool: ...
-def contents(package: Package) -> Iterator[str]: ...
+def files(anchor: Package | None = ...) -> Traversable: ...
 
-if sys.version_info >= (3, 12):
-    def files(anchor: Package | None = ...) -> Traversable: ...
-
-elif sys.version_info >= (3, 9):
-    def files(package: Package) -> Traversable: ...
-
-if sys.version_info >= (3, 10):
-    from importlib.abc import ResourceReader as ResourceReader
+from importlib.abc import ResourceReader as ResourceReader
