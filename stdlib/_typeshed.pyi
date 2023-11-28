a10f00d959f7b2992509d075685180f96879f8e7
AlexWaygood <alex.waygood@gmail.com>
2023-11-28 15:53:04 +0000

    repros

--- stdlib/_typeshed.pyi
+++ stdlib/_typeshed.pyi
@@ -1,12 +1,9 @@
 from typing import Any, Iterable, Protocol, TypeVar
 from typing_extensions import TypeAlias
 
-_T = TypeVar("_T")
 _KT = TypeVar("_KT")
 _VT_co = TypeVar("_VT_co", covariant=True)
 _T_contra = TypeVar("_T_contra", contravariant=True)
-
-Incomplete: TypeAlias = Any
 
 class SupportsDunderLT(Protocol[_T_contra]):
     def __lt__(self, __other: _T_contra) -> bool: ...
@@ -26,12 +23,3 @@ SupportsRichComparisonT = TypeVar("SupportsRichComparisonT", bound=SupportsRichC
 class SupportsKeysAndGetItem(Protocol[_KT, _VT_co]):
     def keys(self) -> Iterable[_KT]: ...
     def __getitem__(self, __key: _KT) -> _VT_co: ...
-
-class IdentityFunction(Protocol):
-    def __call__(self, __x: _T) -> _T: ...
-
-StrPath: TypeAlias = str  # stable
-BytesPath: TypeAlias = bytes  # stable
-StrOrBytesPath: TypeAlias = str | bytes  # stable
-WriteableBuffer: TypeAlias = bytes
-ReadableBuffer: TypeAlias = bytes  # stable
