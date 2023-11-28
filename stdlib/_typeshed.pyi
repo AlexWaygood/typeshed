4efc92ea020ff19c8f5bdc4cb411e6280fa867c3
AlexWaygood <alex.waygood@gmail.com>
2023-11-28 11:13:31 +0000

    Repros

--- stdlib/_typeshed.pyi
+++ stdlib/_typeshed.pyi
@@ -4,7 +4,6 @@
 
 import sys
 from collections.abc import Awaitable, Callable, Iterable, Sequence, Set as AbstractSet, Sized
-from dataclasses import Field
 from os import PathLike
 from types import FrameType, TracebackType
 from typing import Any, AnyStr, ClassVar, Generic, Protocol, SupportsFloat, SupportsInt, TypeVar, overload
@@ -309,13 +308,6 @@ ProfileFunction: TypeAlias = Callable[[FrameType, str, Any], object]
 
 # Objects suitable to be passed to sys.settrace, threading.settrace, and similar
 TraceFunction: TypeAlias = Callable[[FrameType, str, Any], TraceFunction | None]
-
-# experimental
-# Might not work as expected for pyright, see
-#   https://github.com/python/typeshed/pull/9362
-#   https://github.com/microsoft/pyright/issues/4339
-class DataclassInstance(Protocol):
-    __dataclass_fields__: ClassVar[dict[str, Field[Any]]]
 
 # Anything that can be passed to the int/float constructors
 ConvertibleToInt: TypeAlias = str | ReadableBuffer | SupportsInt | SupportsIndex | SupportsTrunc
