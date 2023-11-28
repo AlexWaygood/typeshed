9a7fcc3eed57972b841bba10971dd254b3e53e50
AlexWaygood <alex.waygood@gmail.com>
2023-11-28 13:42:58 +0000

    repros

--- stdlib/_typeshed.pyi
+++ stdlib/_typeshed.pyi
@@ -1,27 +1,12 @@
-# Utility types for typeshed
-#
-# See the README.md file in this directory for more information.
-
 from typing import Any, Iterable, Protocol, TypeVar
 from typing_extensions import Buffer, Literal, TypeAlias
 
 _T = TypeVar("_T")
 _KT = TypeVar("_KT")
 _VT_co = TypeVar("_VT_co", covariant=True)
-_T_co = TypeVar("_T_co", covariant=True)
 _T_contra = TypeVar("_T_contra", contravariant=True)
 
-# For partially known annotations. Usually, fields where type annotations
-# haven't been added are left unannotated, but in some situations this
-# isn't possible or a type is already partially known. In cases like these,
-# use Incomplete instead of Any as a marker. For example, use
-# "Incomplete | None" instead of "Any | None".
 Incomplete: TypeAlias = Any
-
-# To describe a function parameter that is unused and will work with anything.
-Unused: TypeAlias = object
-
-# Comparison protocols
 
 class SupportsDunderLT(Protocol[_T_contra]):
     def __lt__(self, __other: _T_contra) -> bool: ...
@@ -42,9 +27,6 @@ class SupportsAllComparisons(
 SupportsRichComparison: TypeAlias = SupportsDunderLT[Any] | SupportsDunderGT[Any]
 SupportsRichComparisonT = TypeVar("SupportsRichComparisonT", bound=SupportsRichComparison)  # noqa: Y001
 
-# Dunder protocols
-
-# stable
 class SupportsKeysAndGetItem(Protocol[_KT, _VT_co]):
     def keys(self) -> Iterable[_KT]: ...
     def __getitem__(self, __key: _KT) -> _VT_co: ...
@@ -85,14 +67,6 @@ OpenBinaryModeUpdating: TypeAlias = Literal[
 OpenBinaryModeWriting: TypeAlias = Literal["wb", "bw", "ab", "ba", "xb", "bx"]
 OpenBinaryModeReading: TypeAlias = Literal["rb", "br", "rbU", "rUb", "Urb", "brU", "bUr", "Ubr"]
 OpenBinaryMode: TypeAlias = OpenBinaryModeUpdating | OpenBinaryModeReading | OpenBinaryModeWriting
-
-# stable
-class SupportsRead(Protocol[_T_co]):
-    def read(self, __length: int = ...) -> _T_co: ...
-
-# stable
-class SupportsWrite(Protocol[_T_contra]):
-    def write(self, __s: _T_contra) -> object: ...
 
 WriteableBuffer: TypeAlias = Buffer
 ReadableBuffer: TypeAlias = Buffer  # stable
