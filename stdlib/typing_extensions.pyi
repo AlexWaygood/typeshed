077f95696b95176530a543a0b0bfd4dd9dfa6481
AlexWaygood <alex.waygood@gmail.com>
2023-11-28 15:47:50 +0000

    repros

--- stdlib/typing_extensions.pyi
+++ stdlib/typing_extensions.pyi
@@ -1,4 +1,4 @@
 from types import get_original_bases as get_original_bases
-from typing import AnyStr as AnyStr, Never as Never, Self as Self, TypeAlias as TypeAlias, _SpecialForm
+from typing import TypeAlias as TypeAlias, _SpecialForm
 
 Literal: _SpecialForm
