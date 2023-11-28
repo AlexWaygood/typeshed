bdf4a50a635649eb48eecd841704eab0a04096b1
AlexWaygood <alex.waygood@gmail.com>
2023-11-28 15:45:16 +0000

    repros

--- stdlib/typing_extensions.pyi
+++ stdlib/typing_extensions.pyi
@@ -1,20 +1,4 @@
-from collections.abc import Buffer as Buffer
 from types import get_original_bases as get_original_bases
-from typing import (  # noqa: Y038
-    IO as IO,
-    TYPE_CHECKING as TYPE_CHECKING,
-    AbstractSet as AbstractSet,
-    Any as Any,
-    AnyStr as AnyStr,
-    LiteralString as LiteralString,
-    Never as Never,
-    NotRequired as NotRequired,
-    Required as Required,
-    Self as Self,
-    TypeAlias as TypeAlias,
-    TypeGuard as TypeGuard,
-    Unpack as Unpack,
-    _SpecialForm,
-)
+from typing import AnyStr as AnyStr, Never as Never, Self as Self, TypeAlias as TypeAlias, _SpecialForm
 
 Literal: _SpecialForm
