9cebf809cc3e964397dba4659ae1f6eb265e4252
AlexWaygood <alex.waygood@gmail.com>
2023-11-28 16:22:00 +0000

    repros

--- stdlib/builtins.pyi
+++ stdlib/builtins.pyi
@@ -1,6 +1,6 @@
 import collections.abc  # noqa: F401
+import typing_extensions  # noqa: F401
 from typing import Any, MutableMapping, MutableSequence, Sequence, TypeVar  # noqa: Y022
-from typing_extensions import Literal, TypeAlias
 
 SupportsRichComparison = Any
 SupportsRichComparisonT = Any
@@ -27,9 +27,5 @@ class str(Sequence[str]): ...
 class list(MutableSequence[_T]): ...
 class tuple(Sequence[_T_co]): ...
 class dict(MutableMapping[_KT, _VT]): ...
-
-_PositiveInteger: TypeAlias = Literal[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-_NegativeInteger: TypeAlias = Literal[-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16, -17, -18, -19, -20]
-_LiteralInteger = _PositiveInteger | _NegativeInteger | Literal[0]  # noqa: Y026
 
 Ellipsis: ellipsis
